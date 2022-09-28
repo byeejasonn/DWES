5798c9781623011ebb2739808442a6205bb7d112
byeejasonn <jasonlob528@gmail.com>
2022-09-28 23:57:22 +0200

    Un desastre pero funciona

--- UT2/arrayhorario.php
+++ UT2/arrayhorario.php
@@ -8,9 +8,36 @@
     ];
 
     function print_horario($horario) {
-        //$keys = 
+        $keys = array_keys($horario);
+        // $fila = "";
 
-        echo array_keys($horarios);
+
+
+        // for ($i=0; $i < count($horario[key($horario)]); $i++) { 
+        //     for ($j=0; $j < count($horario); $j++) { 
+        //         // echo $horario[$keys[$j][$i]];
+        //         echo $horario[$keys[$j]][$i];
+        //     }
+        //     next($horario);
+        //     echo "<br>";
+        // }
+
+        // foreach ($horario as $dia => $modulos) {
+        //     // print_r($horario[$dia]);
+
+        // ?>
+        // <div class="vertical">
+        // <?php    foreach ($modulos as $index => $modulo) {
+        //         echo "<span>$modulo</span>";
+        //     }
+        // ?>
+        // </div>
+        // <?php
+        //     echo "<br>";
+        // }
+
+        // return $fila;
+
 
         // for ($i=0; !is_null($horario[key($horario)]) && $i < count($horario[key($horario)]); $i++) { 
         //     echo key($horario)." ".count($horario[key($horario)])."<br>";
@@ -29,9 +56,21 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Horario</title>
+    <style>
+        .horario {
+            display: flex;
+            width: fit-content;
+        }
+        .vertical {
+            display: flex;
+            flex-direction: column;
+        }
+    </style>
 </head>
 <body>
     <h2>Horario</h2>
-    <?= print_horario($horario) ?>
+    <div class="horario">
+        <?= print_horario($horario) ?>
+    </div>
 </body>
 </html>
