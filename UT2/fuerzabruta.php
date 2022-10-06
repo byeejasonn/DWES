028dc66d6bd43c1139d821a30d2a3549990852c1
byeejasonn <jasonlob528@gmail.com>
2022-10-06 02:24:35 +0200

    tendre que creerme que funciona

--- UT2/fuerzabruta.php
+++ UT2/fuerzabruta.php
@@ -1,22 +1,24 @@
 <?php
-
+$passwd = '';
 $pass = false;
 
 while(!$pass) {
 
-    for ($i=97; $i < 123; $i++) { 
+    for ($i=97; $i < 123; $i++) {
         for ($j=97; $j < 123; $j++) { 
-            for ($k=97; $k < 123; $k++) { 
+            for ($k=97; $k < 123; $k++) {
                 for ($l=97; $l < 123; $l++) { 
                     $passwd = chr($i).chr($j).chr($k).chr($l);
+                    if(password_verify("$passwd", '$2y$10$0GNiidCkeO/VBBHPH0DP6e5tgz6l/FIOxs1RcFloJrXuTYmmAsW72')){
+                        $pass = true;
+                        break;
+                    }
                 }
             }
         }   
     }
-    if(password_verify("$passwd", '$2y$10$0GNiidCkeO/VBBHPH0DP6e5tgz6l/FIOxs1RcFloJrXuTYmmAsW72')) {
-        echo "Contraseña: $passwd";
-        $pass = true;
-    }
 }
+
+echo "Contraseña: $passwd";
 
 ?>
