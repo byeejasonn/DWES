ecc930992f878fd3b97aac979e6ca2eaf4353d69
Jason Ubuntu clase <jasonlob528@gmail.com>
2022-10-07 18:40:10 +0200

    ejercicio 8

---
+++ UT2/00-enunciados/08-doryanantonionataly.php
@@ -0,0 +1,46 @@
+<?php
+    $persona1 = [
+        "nombre" => "Jason",
+        "apellidos" => "Londoño Barreto",
+        "telefono" => "111222333",
+        "email" => "jason@mail.com"
+    ];
+    $persona2 = [
+        "nombre" => "Mario",
+        "apellidos" => "Sanchez Luque",
+        "telefono" => "333222111",
+        "email" => "mario@mail.com"
+    ];
+    $persona3 = [
+        "nombre" => "Luis Daniel",
+        "apellidos" => "Casado Guimaraes",
+        "telefono" => "123123123",
+        "email" => "daniel@mail.com"
+    ];
+    $persona4 = [
+        "nombre" => "Jovani",
+        "apellidos" => "Vazquez",
+        "telefono" => "321321321",
+        "email" => "jovani@bendicione.com"
+    ];
+
+    $personas = array_merge([$persona1],[$persona2],[$persona3],[$persona4]);
+
+    print_r($personas);
+
+    $edades = ["25","22","24","29","26","23","20","19","26","19","20","23"];
+
+    
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Enunciado Doryan Antonio Nataly</title>
+</head>
+<body>
+
+</body>
+</html>
