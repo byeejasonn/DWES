73ff41a22437da122d5d1c82b76d5af88d9002f4
byeejasonn <jasonlob528@gmail.com>
2022-11-10 21:41:10 +0100

    'merge' mario

---
+++ UT4/proyecto/Form/StudentManager.php
@@ -0,0 +1,61 @@
+<?php
+    namespace Form;
+
+    class StudentManager {
+        private static $list = [];
+        private static $instance;
+
+        public static function singleton() {
+            if(!isset(self::$instance)) {
+                self::$instance = new \Form\StudentManager();
+            }
+            return self::$instance;
+        }
+
+        public function fetchStudents() {
+            $aux = @file_get_contents(
+                "list.csv"
+            );
+            
+            if ($aux != false) {
+                // Si recupera el archivo
+                $aux = explode("\n", $aux);
+                array_pop($aux);
+
+                foreach ($aux as $student) {
+                    $student = explode(",", $student);
+                    // Hardcodear es mi pasión
+                    self::$list[] = new \Form\Student($student[0], $student[1], $student[2], $student[3], $student[4], $student[5], $student[6], $student[7], $student[8]);
+                }
+
+            } else {
+                // Si no existe el archivo, lo crea
+                self::$list = file_put_contents("list.csv","");    
+            }
+
+            return self::$list;
+        }
+
+        public function saveAlumnos(\Form\Student $student) {
+            file_put_contents(
+                "list.csv",
+                $student->getName().",".
+                $student->getSurname().",".
+                $student->getUser().",".
+                $student->getPassword().",".
+                $student->getMail().",".
+                $student->getPhone().",".
+                $student->getGender().",".
+                $student->getBirthdate().",".
+                $student->getGrade()."\n",
+                FILE_APPEND
+            );
+        }
+
+        static function printList() {
+            foreach (self::$list as $alumno) {
+                echo @$alumno->__toString();
+            }
+        }
+    }
+?>
