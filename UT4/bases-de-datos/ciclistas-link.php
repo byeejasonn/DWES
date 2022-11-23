a811216635b5894c935082b7620303bcae2736f4
byeejasonn <jasonlob528@gmail.com>
2022-11-24 00:06:36 +0100

    ciclistas listas done

--- UT4/bases-de-datos/ciclistas-link.php
+++ UT4/bases-de-datos/ciclistas-link.php
@@ -1,28 +1,26 @@
 <?php
-
-    $dsn = 'mysql:host=localhost;dbname=dwes';
-    $user = 'byeejasonn';
-    $passwd = '1234';
-    $options = [
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-    ];
-
-    $conn = new PDO($dsn, $user, $passwd, $options);
-
-    $stmt = $conn->query("SELECT * FROM Ciclistas");
-
-    imprimirLista($stmt);
-
-    $stmt = null;
-    $conn = null;
+    try {
+        $dsn = 'mysql:host=localhost;dbname=dwes';
+        $user = 'byeejasonn';
+        $passwd = '1234';
+        $options = [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ];
     
-
-
-
+        $conn = new PDO($dsn, $user, $passwd, $options);
+    
+        $stmt = $conn->query("SELECT * FROM Ciclistas");
+    
+    } catch (PDOException $e) {
+        echo "¡Error!: " . $e->getMessage() . "\n";
+        die();
+    }
+    
     function imprimirLista($stmt) {
         $result = $stmt->fetchAll();
 ?> 
-        <ul> 
+        <h3>Listado ciclistas</h3>
+        <ul class="ciclistas"> 
             <?php foreach ($result as $key => $value) : ?>
                 <li>
                     <a href="detalles.php?id=<?= $value['id'] ?>"><?= $value['nombre'] ?></a>
@@ -39,8 +37,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Listado ciclistas</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+    <div class="main">
+        <?php imprimirLista($stmt); ?>
+    </div>
+
 </body>
 </html>
--- UT4/bases-de-datos/detalles.php
+++ UT4/bases-de-datos/detalles.php
@@ -1,29 +1,31 @@
 <?php
-    $dsn = 'mysql:host=localhost;dbname=dwes';
-    $user = 'byeejasonn';
-    $passwd = '1234';
-    $options = [
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-    ];
-
-    $conn = new PDO($dsn, $user, $passwd, $options);
-
-    $stmt = $conn->query("SELECT nombre, num_trofeos FROM Ciclistas WHERE id = :id");
-    $stmt->bindParam(':id', $_GET['id']);
-
-    imprimirDetalles($stmt);
-
-    $stmt = null;
-    $conn = null;
+    try {
+        
+        $dsn = 'mysql:host=localhost;dbname=dwes';
+        $user = 'byeejasonn';
+        $passwd = '1234';
+        $options = [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ];
+    
+        $conn = new PDO($dsn, $user, $passwd, $options);
+    
+        $stmt = $conn->prepare("SELECT nombre, num_trofeos FROM Ciclistas WHERE id = :id");
+        $stmt->bindParam(':id', $_GET['id']);
+    
+    } catch (PDOException $e) {
+        print "¡Error!: " . $e->getMessage() . "\n";
+        die();
+    }
 
     function imprimirDetalles($stmt) {
-        $result = $stmt->fetchAll();
+        $stmt->execute();
 
-        foreach ($result as $key => $value) : ?>
-            <p>El ciclista <?= $value['nombre'] ?> tiene 
+        foreach ($stmt->fetchAll() as $key => $value) : ?>
+            <p>El ciclista <b><?= $value['nombre'] ?></b> tiene 
 
-                <?php for ($i=0; $i < count($value['num_trofeos']); $i++) : ?>
-                    1
+                <?php for ($i=0; $i < $value['num_trofeos']; $i++) : ?>
+                    <img src="./trophy-solid.svg" alt="trohpy" class='icon' draggable='false'>
                 <?php endfor; ?>
             </p>       
 <?php   endforeach;
@@ -37,8 +39,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Detalles Ciclista</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+    <div class="main">
+        <h3>Ciclista</h3>
+        <?php imprimirDetalles($stmt); ?>
+        <a href="./ciclistas-link.php">Atras</a>
+    </div>
 </body>
 </html>
