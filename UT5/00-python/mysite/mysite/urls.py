2e0dd4cb6ca194f1d76e7f9ff94658fb9e69ae70
byeejasonn <jasonlob528@gmail.com>
2023-02-10 18:41:41 +0100

    static files production

--- UT5/00-python/mysite/mysite/urls.py
+++ UT5/00-python/mysite/mysite/urls.py
@@ -15,13 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+# from django.conf import settings
+# from django.conf.urls.static import static
 
 from . import views
 
 app_name = 'mysite'
 urlpatterns = [
-    path('', views.index, name="index"),
+    # path('', views.index, name="index"),
+    path('', include('polls.urls'), name="polls"),
     path('admin/', admin.site.urls, name="admin"),
-    path('polls/', include('polls.urls'), name="polls"),
     path('twitter/', include('twitter.urls'), name="twitter"),
-]
+] 
+
+# + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
